3e90224fd044987a79ec70b2bea0dec4ce74fd45
ea-open-source <open-source@eastagile.com>
2016-05-30 14:59:14 +0700

    [r] Fix react-router warning

--- src/components/App.react.js
+++ src/components/App.react.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Route, Router} from 'react-router';
-import {useBasename, createHistory} from 'history';
+import Link from 'react-router/lib/Link'
+import Route from 'react-router/lib/Route'
+import Router from 'react-router/lib/Router'
 // import ComplaintPage from 'components/ComplaintPage.react';
 import MainPage from 'components/MainPage.react';
 import NoMatch from 'components/NoMatch.react';
@@ -10,16 +11,17 @@ import style from 'styles/Style.sass';
 import fonts from 'styles/Fonts.sass';
 import grid from 'styles/Grid.sass';
 import helper from 'styles/Helper.sass';
+import useRouterHistory from 'react-router/lib/useRouterHistory';
+import { createHistory } from 'history'
 
-const browserHistory = useBasename(createHistory)({
-    basename: "/"
+const history = useRouterHistory(createHistory)({
+  basename: '/'
 });
-
 
 const App = React.createClass({
   render() {
     return (
-      <Router history={ browserHistory }>
+      <Router history={ history }>
         <Route path='/s/:query' component={ MainPage } />
         <Route path='/q/:query' component={ MainPage } />
         <Route path='/' component={ MainPage } />
