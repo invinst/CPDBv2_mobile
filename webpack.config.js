3668cb7de5c29eccf2c43a592953703fb50c2dbc
ea-open-source <open-source@eastagile.com>
2016-09-08 18:00:21 +0700

    [r] Use node's phantoms instead

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,6 @@ process.env.REACT_WEBPACK_ENV = env;
 function buildConfig(wantedEnv) {
   let isValid = wantedEnv && wantedEnv.length > 0 && allowedEnvs.indexOf(wantedEnv) !== -1;
   let validEnv = isValid ? wantedEnv : 'dev';
-  console.log(validEnv);
   let config = require(path.join(__dirname, 'cfg/' + validEnv));
   return config;
 }
