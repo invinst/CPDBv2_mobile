1128b1a563b2251c7978a11f5199c1bd7fb3df50
ea-open-source <open-source@eastagile.com>
2016-09-23 10:44:21 +0700

    [r] Fix staging configuration (4)
    [#130628617] https://www.pivotaltracker.com/story/show/130628617

--- src/config/dist.js
+++ src/config/dist.js
@@ -5,7 +5,7 @@ import baseConfig from './base';
 let config = {
   appEnv: 'dist',  // feel free to remove the appEnv property here
   baseUrlV1: 'http://m.cpdb.co',
-  baseUrlV2: 'https://beta.cpdp.co/api/v1'
+  baseUrlV2: 'http://staging.cpdp.co/api/v1'
 };
 
 export default Object.freeze(Object.assign({}, baseConfig, config));
