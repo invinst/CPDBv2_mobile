39fb51df3aa190c3295b393ccfa9037587a1560f
ea-open-source <open-source@eastagile.com>
2017-04-26 11:54:25 +0700

    [c] 489 - Remove v1Url helper

--- src/utils/UrlUtil.js
+++ src/utils/UrlUtil.js
@@ -1,14 +1,6 @@
 import S from 'string';
 import config from 'config';
 
-
-export const v1Url = function (endpoint) {
-  return S('{{baseUrl}}{{endpoint}}').template({
-    'baseUrl': config.baseUrlV1,
-    'endpoint': endpoint
-  }).s;
-};
-
 export const v2Url = function (endpoint) {
   return S('{{baseUrl}}{{endpoint}}').template({
     'baseUrl': config.baseUrlV2,
