bd759e3a981c978dba0765ea893f0aa7cbafaf12
ea-open-source <open-source@eastagile.com>
2018-05-18 17:41:05 +0700

    [r] 671c - Recover SimpleList test
    [#157624227] https://www.pivotaltracker.com/story/show/157624227

--- test/components/Shared/SimpleList.spec.js
+++ test/components/Shared/SimpleList.spec.js
@@ -1,10 +1,71 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { spy } from 'sinon';
+import { Link } from 'react-router';
+import constants from 'constants';
 
 import SimpleList from 'components/Shared/SimpleList';
 
 describe('<SimpleList />', function () {
+  it('should render label', function () {
+
+    const rows = [
+      {
+        label: 'Result 1',
+        onClick: (() => {}).bind(undefined, {
+          type: 'RESULT',
+          title: 'Result 1',
+          url: 'result/1'
+        })
+      },
+      {
+        label: 'Result 2',
+        onClick: (() => {}).bind(undefined, {
+          type: 'RESULT',
+          title: 'Result 2',
+          url: 'result/1'
+        })
+      }
+    ];
+    const wrapper = shallow(<SimpleList rows={ rows } />);
+    const labels = wrapper.find('.label');
+
+    labels.should.have.length(2);
+    labels.at(0).text().should.eql('Result 1');
+    labels.at(1).text().should.eql('Result 2');
+
+  });
+
+  it('should render Link tag with proper onClick() if url is provided', function () {
+    const onClick1 = spy();
+    const onClick2 = spy();
+
+    const rows = [
+      {
+        label: 'Result 1',
+        url: 'result/1',
+        onClick: onClick1
+      },
+      {
+        label: 'Result 2',
+        url: 'result/2',
+        onClick: onClick2
+      }
+    ];
+    const wrapper = mount(<SimpleList rows={ rows } />);
+    const links = wrapper.find(Link);
+
+    links.should.have.length(2);
+
+    links.at(0).text().should.eql('Result 1');
+    links.at(0).prop('to').should.eql('result/1');
+    links.at(0).prop('onClick').should.equal(onClick1);
+
+    links.at(1).text().should.eql('Result 2');
+    links.at(1).prop('to').should.eql('result/2');
+    links.at(1).prop('onClick').should.equal(onClick2);
+  });
+
   it('should render div tag with proper onClick() if url is NOT provided', function () {
     const onClick1 = spy();
     const onClick2 = spy();
