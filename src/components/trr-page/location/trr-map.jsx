92699efb48f2528b32b345070b2d11e3ff830c2c
ea-open-source <open-source@eastagile.com>
2018-06-14 16:18:37 +0700

    [f] 435d - Add location map into trr page
    [#158107511](https://www.pivotaltracker.com/story/show/158107511)

---
+++ src/components/trr-page/location/trr-map.jsx
@@ -0,0 +1,31 @@
+import React, { PropTypes, Component } from 'react';
+
+import style from 'styles/trr-page/location/trr-map.sass';
+import LocationMap from 'components/common/location-map';
+
+
+export default class TRRMap extends Component {
+
+  render() {
+    const { lng, lat } = this.props;
+    const marker = (
+      <div className={ style.trrMapMarker }>
+        <div className='outer-circle'/>
+      </div>
+    );
+
+    return (
+      <LocationMap
+        lat={ lat }
+        lng={ lng }
+        mapboxStyle='mapbox://styles/mapbox/light-v9'
+        markerEl={ marker }
+      />
+    );
+  }
+}
+
+TRRMap.propTypes = {
+  lat: PropTypes.number,
+  lng: PropTypes.number
+};
