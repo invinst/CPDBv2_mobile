7d0f34572f57fde6c0713b23d15580e6037c1319
ea-open-source <open-source@eastagile.com>
2018-07-02 17:02:16 +0700

    [c] fix live tests

--- live-tests/test/main-page.spec.js
+++ live-tests/test/main-page.spec.js
@@ -1,7 +1,31 @@
 'use strict';
 
+var api = require(__dirname + '/../mock-api');
+
+const mockLandingPageCms = {
+  fields: [
+    {
+      name: 'navbar_title',
+      type: 'rich_text',
+      value: {
+        entityMap: {},
+        blocks: [{
+          data: {},
+          depth: 0,
+          entityRanges: [],
+          inlineStyleRanges: [],
+          key: '2ff82',
+          text: 'Citizens Police Data Project',
+          type: 'unstyled'
+        }]
+      }
+    }
+  ]
+};
+
 describe('MainPageTest', function () {
   beforeEach(function (client, done) {
+    api.mock('GET', '/api/v2/cms-pages/landing-page/', 200, mockLandingPageCms);
     this.mainPage = client.page.main();
     this.mainPage.navigate();
     done();
@@ -16,7 +40,7 @@ describe('MainPageTest', function () {
   it('should show homepage with logo and navigation links', function (client) {
     const mainPage = this.mainPage;
 
-    mainPage.expect.element('@cpdbLogo').text.to.contain('Citizens Police Data Project');
+    mainPage.expect.element('@title').text.to.contain('Citizens Police Data Project');
     mainPage.expect.element('@searchLink').to.be.visible;
   });
 
