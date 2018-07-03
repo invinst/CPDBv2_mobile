0cda8d64b067a53c02c7ed7a23dde86b56d48970
ea-open-source <open-source@eastagile.com>
2018-07-03 19:11:58 +0700

    [r] Add the middlewares to configure-store.prod

--- src/stores/configure-store.prod.js
+++ src/stores/configure-store.prod.js
@@ -2,15 +2,17 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import persistState from 'redux-localstorage';
 
+import configuredAxiosMiddleware from 'middleware/configured-axios-middleware';
 import rootReducer from 'reducers/root-reducer';
 import localStorageConfig from './local-storage-config';
+import scrollPositionMiddleware from '../middleware/scroll-position-middleware';
 
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
     compose(
-      applyMiddleware(thunk),
+      applyMiddleware(thunk, configuredAxiosMiddleware, scrollPositionMiddleware),
       persistState(()=>{}, localStorageConfig)
     )
   );
