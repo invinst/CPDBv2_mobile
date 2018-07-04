613696386a52d52c0c0f47fc1749c4a5489ee643
ea-open-source <open-source@eastagile.com>
2018-07-04 18:01:29 +0700

    [f] 995 - Add more test for officer profile page
    [#158106995] https://www.pivotaltracker.com/story/show/158106995

--- live-tests/test/officer-page.spec.js
+++ live-tests/test/officer-page.spec.js
@@ -47,11 +47,136 @@ const officer2235 = {
   ]
 };
 
+const mockTimeline = [
+  {
+    'unit_name': '007',
+    kind: 'AWARD',
+    'unit_description': 'District 007',
+    rank: 'Police Officer',
+    date: '2006-03-01',
+    'award_type': 'Honorable Mention'
+  },
+  {
+    'trr_id': 1,
+    'unit_name': '007',
+    kind: 'FORCE',
+    taser: true,
+    'unit_description': 'District 007',
+    rank: 'Police Officer',
+    date: '2005-12-17',
+    'firearm_used': false
+  },
+  {
+    'trr_id': 2,
+    'unit_name': '007',
+    kind: 'FORCE',
+    taser: false,
+    'unit_description': 'District 007',
+    rank: 'Police Officer',
+    date: '2005-03-17',
+    'firearm_used': false
+  },
+  {
+    'unit_name': '007',
+    kind: 'UNIT_CHANGE',
+    'unit_description': 'District 007',
+    rank: 'Police Officer',
+    date: '2005-01-07'
+  },
+  {
+    'trr_id': 3,
+    'unit_name': '153',
+    kind: 'FORCE',
+    taser: false,
+    'unit_description': 'Mobile Strike Force',
+    rank: 'Police Officer',
+    date: '2004-12-17',
+    'firearm_used': true
+  },
+  {
+    category: 'Illegal Search',
+    'unit_name': '153',
+    kind: 'CR',
+    subcategory: 'Search Of Premise Without Warrant',
+    crid: '294088',
+    'unit_description': 'Mobile Strike Force',
+    rank: 'Police Officer',
+    date: '2003-11-26',
+    coaccused: 8,
+    finding: 'Exonerated',
+    outcome: 'No Action Taken',
+    attachments: [
+      {
+        url: 'https://www.documentcloud.org/documents/3518950-CRID-294088-CR.html',
+        'preview_image_url': 'https://assets.documentcloud.org/documents/3518950/pages/CRID-294088-CR-p1-normal.gif',
+        title: 'CRID 294088 CR',
+        'file_type': 'document',
+      },
+      {
+        url: 'https://player.vimeo.com/video/165206078',
+        title: 'Video Clip',
+        'file_type': 'video',
+      },
+      {
+        url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/262463136&amp',
+        title: 'Audio Clip',
+        'file_type': 'audio',
+      }
+    ]
+  },
+  {
+    category: 'Criminal Misconduct',
+    'unit_name': '153',
+    kind: 'CR',
+    subcategory: 'Theft',
+    crid: '260131',
+    'unit_description': 'Mobile Strike Force',
+    rank: 'Police Officer',
+    date: '2003-02-17',
+    coaccused: 4,
+    finding: 'Unfounded',
+    outcome: 'No Action Taken',
+    attachments: [
+      {
+        url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/262463125&amp',
+        title: 'Audio Clip',
+        'file_type': 'audio',
+      },
+      {
+        url: 'https://player.vimeo.com/video/165213573',
+        title: 'Video Clip',
+        'file_type': 'video',
+      },
+      {
+        url: 'https://www.documentcloud.org/documents/3518954-CRID-299780-CR.html',
+        'preview_image_url': 'https://assets.documentcloud.org/documents/3518954/pages/CRID-299780-CR-p1-normal.gif',
+        title: 'CRID 294088 CR',
+        'file_type': 'document',
+      }
+    ]
+  },
+  {
+    'unit_name': '153',
+    kind: 'UNIT_CHANGE',
+    'unit_description': 'Mobile Strike Force',
+    rank: 'Police Officer',
+    date: '2000-04-28'
+  },
+  {
+    'unit_name': '044',
+    kind: 'JOINED',
+    'unit_description': 'Recruit Training Section',
+    rank: 'Police Officer',
+    date: '2000-02-05'
+  }
+];
+
 
 describe('OfficerPage test', function () {
-
   beforeEach(function (client, done) {
+    this.client = client;
     api.mock('GET', '/api/v2/mobile/officers/2235/', 200, officer2235);
+    api.mock('GET', '/api/v2/mobile/officers/2235/new-timeline-items/', 200, mockTimeline);
 
     this.officerPage = client.page.officerPage();
     this.officerPage.navigate(this.officerPage.url(2235));
@@ -138,5 +263,31 @@ describe('OfficerPage test', function () {
     honorableMentionItem.expect.element('@value').text.to.equal('55');
     honorableMentionItem.expect.element('@name').text.to.equal('Honorable Mentions');
     honorableMentionItem.expect.element('@description').text.to.equal('More than 85% of other officers');
+  });
+
+  describe('Timeline', function () {
+    beforeEach(function (client, done) {
+      this.timeline = this.officerPage.section.timeline;
+      done();
+    });
+
+    it('should go to cr page when clicking on an cr timeline item', function () {
+      this.timeline.click('@crItem');
+      this.timeline.assert.urlContains('/complaint/294088/2235/');
+    });
+
+    it('should go to attachment source page when clicking on the attachment thumbnail', function () {
+      this.timeline.click('@attachmentThumbnail');
+      this.client.windowHandles(result => {
+        const handles = result.value;
+        this.client.switchWindow(handles[handles.length - 1]);
+      });
+      this.client.assert.urlEquals('https://www.documentcloud.org/documents/3518950-CRID-294088-CR.html');
+    });
+
+    it('should go to trr page when clicking on an trr timeline item', function () {
+      this.timeline.click('@trrItem');
+      this.timeline.assert.urlContains('/trr/1/');
+    });
   });
 });
