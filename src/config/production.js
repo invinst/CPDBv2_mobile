7a6b199d18627cc36ac66aa4c1ee7dabeae79c6e
ea-open-source <open-source@eastagile.com>
2018-08-23 12:31:00 +0700

    [f] 337 - Update google analytics ID
    [#159981337](https://www.pivotaltracker.com/story/show/159981337)

--- src/config/production.js
+++ src/config/production.js
@@ -6,7 +6,8 @@ let config = {
   appEnv: 'production',
   baseUrlV1: 'http://m.cpdb.co',
   baseUrlV2: 'https://beta.cpdp.co/api/v1',
-  baseUrlV2V2: 'https://beta.cpdp.co/api/v2'
+  baseUrlV2V2: 'https://beta.cpdp.co/api/v2',
+  gaTrackingId: 'UA-63671047-2'
 };
 
 export default Object.freeze(Object.assign({}, baseConfig, config));
