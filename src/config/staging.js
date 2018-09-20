2c26fb7130e653563ede997929e385997e88a1aa
ea-open-source <open-source@eastagile.com>
2018-09-20 10:59:58 +0700

    [f] 205 - Set up https for v2 staging
    [#160609205] https://www.pivotaltracker.com/story/show/160609205

--- src/config/staging.js
+++ src/config/staging.js
@@ -5,8 +5,8 @@ import baseConfig from './base';
 let config = {
   appEnv: 'staging',  // feel free to remove the appEnv property here
   baseUrlV1: 'http://m.cpdb.co',
-  baseUrlV2: 'http://staging.cpdp.co/api/v1',
-  baseUrlV2V2: 'http://staging.cpdp.co/api/v2',
+  baseUrlV2: 'https://staging.cpdp.co/api/v1',
+  baseUrlV2V2: 'https://staging.cpdp.co/api/v2',
   gaTrackingId: 'UA-63671047-1',
   intercomAppId: 'gbsby1ik'
 };
