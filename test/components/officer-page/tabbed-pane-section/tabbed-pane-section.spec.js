2d03f6d7d3238a41c107cca01fabc17a8fb38fa5
ea-open-source <open-source@eastagile.com>
2018-11-14 12:00:32 +0700

    [f] 787 - Improve test coverage
    [#158584787] (https://www.pivotaltracker.com/story/show/158584787)

--- test/components/officer-page/tabbed-pane-section/tabbed-pane-section.spec.js
+++ test/components/officer-page/tabbed-pane-section/tabbed-pane-section.spec.js
@@ -64,9 +64,9 @@ describe('TabbedPaneSection component', function () {
   });
 
   it('should render coaccusals tab', function () {
-    const wrapper = shallow(
+    const wrapper = mount(
       <Provider store={ store }>
-        <TabbedPaneSection currentTab={ OFFICER_PAGE_TAB_NAMES.COACCUSALS }/>
+        <TabbedPaneSection currentTab={ OFFICER_PAGE_TAB_NAMES.COACCUSALS } hasCoaccusal={ true } />
       </Provider>
     );
 
