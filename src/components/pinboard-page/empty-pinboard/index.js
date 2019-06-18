6cf83705ce5b8f610fbdae4ff869330b44cda28c
ea-open-source <open-source@eastagile.com>
2019-06-18 16:41:33 +0700

    [f] 505 - Dynamic example pinboards based on backend response
    [#166490505] https://www.pivotaltracker.com/story/show/166490505

---
+++ src/components/pinboard-page/empty-pinboard/index.js
@@ -0,0 +1,37 @@
+import React, { PropTypes, Component } from 'react';
+
+import styles from './empty-pinboard.sass';
+
+import ExamplePinboardLink from 'components/pinboard-page/empty-pinboard/example-pinboard-link';
+
+export default class EmptyPinboardPage extends Component {
+  render() {
+    const { examplePinboards } = this.props;
+    return (
+      <div className={ styles.emptyPinboard }>
+        <div className='empty-pinboard-title'>Add</div>
+        <div className='empty-pinboard-description'>
+          <div>
+            Add officers, or complaint records through search.<br />
+            <br />
+            Or use an example pinboard as a baseline to get started.
+          </div>
+        </div>
+        { examplePinboards.map(pinboard => (
+          <ExamplePinboardLink
+            key={ pinboard.id }
+            id={ pinboard.id }
+            title={ pinboard.title }
+            description={ pinboard.description }
+          />
+        )) }
+        <div className='arrow-head'/>
+        <div className='arrow-shaft'/>
+      </div>
+    );
+  }
+}
+
+EmptyPinboardPage.propTypes = {
+  examplePinboards: PropTypes.array,
+};
--- src/components/pinboard-page/index.js
+++ src/components/pinboard-page/index.js
@@ -28,6 +28,7 @@ export default class PinboardPage extends Component {
       params,
       initialRequested,
       isEmptyPinboard,
+      examplePinboards,
     } = this.props;
 
     if (!initialRequested) {
@@ -35,7 +36,7 @@ export default class PinboardPage extends Component {
     }
 
     if (isEmptyPinboard) {
-      return EmptyPinboard;
+      return <EmptyPinboard examplePinboards={ examplePinboards } />;
     }
 
     return (
@@ -83,6 +84,7 @@ PinboardPage.propTypes = {
   hasMapMarker: PropTypes.bool,
   initialRequested: PropTypes.bool,
   isEmptyPinboard: PropTypes.bool,
+  examplePinboards: PropTypes.array,
 };
 
 PinboardPage.defaultProps = {
