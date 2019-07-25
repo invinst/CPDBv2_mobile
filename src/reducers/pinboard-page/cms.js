aad2fb77122db28b5f120cc4dcda8b24d75bb631
ea-open-source <open-source@eastagile.com>
2019-07-25 17:59:51 +0700

    [f] 606 - no longer handling CMS_REQUEST_FAILURE with noop
    [#167504606] https://www.pivotaltracker.com/story/show/167504606

--- src/reducers/pinboard-page/cms.js
+++ src/reducers/pinboard-page/cms.js
@@ -1,9 +1,8 @@
 import { handleActions } from 'redux-actions';
 
-import { PINBOARD_PAGE_CMS_REQUEST_SUCCESS, PINBOARD_PAGE_CMS_REQUEST_FAILURE } from 'actions/pinboard';
+import { PINBOARD_PAGE_CMS_REQUEST_SUCCESS } from 'actions/pinboard';
 
 
 export default handleActions({
   [PINBOARD_PAGE_CMS_REQUEST_SUCCESS]: (state, action) => action.payload.fields,
-  [PINBOARD_PAGE_CMS_REQUEST_FAILURE]: state => state
 }, []);
