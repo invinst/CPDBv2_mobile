e1730a795e19e1b54c35e6ac62a02e0a28e0757f
ea-open-source <open-source@eastagile.com>
2019-12-03 16:41:31 +0700

    [c] Refactor waitForAnimationEnd command

--- live-tests/utils/waitForAnimationEnd.js
+++ live-tests/utils/waitForAnimationEnd.js
@@ -1,57 +1,59 @@
-const util = require('util');
 const events = require('events');
 const assert = require('assert');
 
-
 const CHECK_INTERVAL = 100;
 
-function checker(previousPosition, currentPosition) {
+function isSamePosition(previousPosition, currentPosition) {
   return previousPosition && previousPosition.x === currentPosition.x && previousPosition.y === currentPosition.y;
 }
 
-function WaitForAnimationEnd() {
-  events.EventEmitter.call(this);
-  this.startTimeInMilliseconds = null;
-}
-
-util.inherits(WaitForAnimationEnd, events.EventEmitter);
-
-WaitForAnimationEnd.prototype.command = function (element, timeoutInMilliseconds) {
-  this.startTimeInMilliseconds = new Date().getTime();
-  const self = this;
-
-  if (typeof timeoutInMilliseconds !== 'number') {
-    timeoutInMilliseconds = this.api.globals.waitForConditionTimeout;
+class WaitForAnimationEnd extends events.EventEmitter {
+  constructor() {
+    super();
+    this.startTimeInMilliseconds = null;
   }
 
-  this.check(element, function (result) {
-    assert.equal(
-      result,
-      true,
-      'waitForAnimationEnd: ' + element + '. Expression wasn\'t true in ' + timeoutInMilliseconds + ' ms.'
-    );
-    self.emit('complete');
-  }, timeoutInMilliseconds);
+  command(element, timeoutInMilliseconds) {
+    this.startTimeInMilliseconds = new Date().getTime();
 
-  return this;
-};
-
-WaitForAnimationEnd.prototype.check = function (element, callback, maxTimeInMilliseconds, currentPosition = null) {
-  const self = this;
-
-  this.api.getLocation(element, function (result) {
-    const now = new Date().getTime();
-    if (result.status === 0 && checker(currentPosition, result.value)) {
-      callback(true, now);
-    } else if (now - self.startTimeInMilliseconds < maxTimeInMilliseconds) {
-      this.currentPosition = result.value;
-      setTimeout(function () {
-        self.check(element, callback, maxTimeInMilliseconds, result.value);
-      }, CHECK_INTERVAL);
-    } else {
-      callback(false);
+    if (typeof timeoutInMilliseconds !== 'number') {
+      timeoutInMilliseconds = this.api.globals.waitForConditionTimeout;
     }
-  });
-};
+
+    this.repeatedlyCheckPosition(
+      element,
+      result => {
+        assert.equal(
+          result,
+          true,
+          `waitForAnimationEnd: ${element}. Expression wasn't true in ${timeoutInMilliseconds} ms.`
+        );
+        this.emit('complete');
+      },
+      timeoutInMilliseconds
+    );
+
+    return this;
+  }
+
+  repeatedlyCheckPosition(element, callback, maxTimeInMilliseconds, previousPosition = null) {
+    this.api.getLocation(
+      element,
+      result => {
+        const now = new Date().getTime();
+        if (result.status === 0 && isSamePosition(previousPosition, result.value)) {
+          callback(true);
+        } else if (now - this.startTimeInMilliseconds < maxTimeInMilliseconds) {
+          setTimeout(
+            () => this.repeatedlyCheckPosition(element, callback, maxTimeInMilliseconds, result.value),
+            CHECK_INTERVAL
+          );
+        } else {
+          callback(false);
+        }
+      }
+    );
+  }
+}
 
 module.exports = WaitForAnimationEnd;
