70540c02d72cf942afdebc5c1da678a31ed0b24d
ea-open-source <open-source@eastagile.com>
2020-02-04 16:10:21 +0700

    [f] Upgrade to react 15.6

--- test/setup.js
+++ test/setup.js
@@ -1,9 +1,12 @@
 import 'should-sinon';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
 import sinon from 'sinon';
 
 window.Intercom = function () {};
 window.ga = function () {};
 window.clicky = { log: function () {} };
+configure({ adapter: new Adapter() });
 
 mocha.suite.afterEach(function () {
   sinon.restore();
